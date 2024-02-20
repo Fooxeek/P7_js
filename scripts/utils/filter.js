cb60fa98452bacb82c6040503da2962af5d56642
Fooxeek <foxeek02@gmail.com>
2024-02-20 15:10:13 +0400

    fix: no-result

--- scripts/utils/filter.js
+++ scripts/utils/filter.js
@@ -8,60 +8,102 @@ import { updateUstensilsList } from "../template/ustensiles.js";
 export let currentFilteredRecipes = [...recipes]; // Commencez avec toutes les recettes
 
 export function filterRecipes(query) {
-  currentFilteredRecipes = recipes.filter((recipe) => {
-    let titleMatch = recipe.name.toLowerCase().includes(query.toLowerCase());
-    let descriptionMatch = recipe.description
+  if (query.length < 3) {
+    displayFilteredRecipes(currentFilteredRecipes);
+    return currentFilteredRecipes;
+  }
+
+  const filteredRecipes = recipes.filter((recipe) => {
+    const titleMatch = recipe.name.toLowerCase().includes(query.toLowerCase());
+    const descriptionMatch = recipe.description
       .toLowerCase()
       .includes(query.toLowerCase());
-    let ingredientMatch = recipe.ingredients.some((ingredient) =>
+    const ingredientMatch = recipe.ingredients.some((ingredient) =>
       ingredient.ingredient.toLowerCase().includes(query.toLowerCase())
     );
 
     return titleMatch || descriptionMatch || ingredientMatch;
   });
 
-  // Mettez à jour l'affichage des recettes ici
-  displayFilteredRecipes(currentFilteredRecipes);
-  updateIngredientsList();
-  updateAppareilsList();
-  updateUstensilsList();
-
-  return currentFilteredRecipes;
-}
-export function filterRecipesBySelections(selections) {
-  if (selections.appareils.size > 1) {
-    return [];
+  if (filteredRecipes.length === 0) {
+    const recipesCard = document.getElementById("recipes__cards");
+    const noResultMessage = document.createElement("div");
+    noResultMessage.textContent = `Aucune recette ne correspond à "${query}"`;
+    noResultMessage.classList.add("no-result");
+    recipesCard.innerHTML = ""; // Supprimez le contenu précédent
+    recipesCard.appendChild(noResultMessage);
+  } else {
+    displayFilteredRecipes(filteredRecipes);
+    updateIngredientsList();
+    updateAppareilsList();
+    updateUstensilsList();
   }
 
-  const filteredRecipes = currentFilteredRecipes.filter(
-    (recipe) =>
+  return filteredRecipes;
+}
+
+export function filterRecipesBySelections(selections) {
+  const filteredRecipes = currentFilteredRecipes.filter((recipe) => {
+    if (selections.appareils.size > 1) {
+      return null;
+    }
+    return (
       filterByIngredients(recipe, selections.ingredients) &&
       filterByAppareils(recipe, selections.appareils) &&
       filterByUstensiles(recipe, selections.ustensiles)
-  );
+    );
+  });
 
+  // Afficher les recettes filtrées
   displayFilteredRecipes(filteredRecipes);
+
+  // Mise à jour du nombre de recettes
   updateRecipeCount(filteredRecipes.length);
+
   return filteredRecipes;
 }
 
 function filterByAppareils(recipe, selectedAppareils) {
-  // Si aucun appareil n'est sélectionné, la recette passe le filtre
+  // Si aucun appareil n'est sélectionné, toutes les recettes passent le filtre
   if (selectedAppareils.size === 0) return true;
-  // La recette passe le filtre si son appareil est dans les appareils sélectionnés
-  return selectedAppareils.has(recipe.appliance);
+
+  // Parcourir chaque appareil sélectionné pour vérifier si la recette actuelle a cet appareil
+  for (const appareil of selectedAppareils) {
+    if (recipe.appliance === appareil) {
+      return true;
+    }
+  }
+  return false;
 }
 
 function filterByIngredients(recipe, selectedIngredients) {
-  return [...selectedIngredients].every((selectedIngredient) =>
-    recipe.ingredients.some(
-      (ingredient) => ingredient.ingredient === selectedIngredient
-    )
-  );
+  if (selectedIngredients.size === 0) return true;
+
+  for (const ingredient of selectedIngredients) {
+    let ingredientFound = false;
+    for (const recipeIngredient of recipe.ingredients) {
+      if (recipeIngredient.ingredient === ingredient) {
+        ingredientFound = true;
+        break;
+      }
+    }
+    if (!ingredientFound) return false;
+  }
+  return true;
 }
 
 function filterByUstensiles(recipe, selectedUstensiles) {
-  return [...selectedUstensiles].every((selectedUstensile) =>
-    recipe.ustensils.includes(selectedUstensile)
-  );
+  if (selectedUstensiles.size === 0) return true;
+
+  for (const ustensile of selectedUstensiles) {
+    let ustensileFound = false;
+    for (const recipeUstensile of recipe.ustensils) {
+      if (recipeUstensile === ustensile) {
+        ustensileFound = true;
+        break;
+      }
+    }
+    if (!ustensileFound) return false;
+  }
+  return true;
 }
